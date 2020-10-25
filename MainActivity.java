1b96951ba6dc03c22586d42a8e1963d0487333bd
ShreyaJamsandekar <67119942+ShreyaJamsandekar@users.noreply.github.com>
2020-10-25 14:25:12 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.touristsguide;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteOpenHelper;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd, btnViewData;
+    private EditText editText ,editEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+
+        editEmail=(EditText) findViewById(R.id.editEmail);   //here
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnViewData = (Button) findViewById(R.id.btnView);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry1 = editText.getText().toString();
+
+                String newEntry2 = editEmail.getText().toString();  //here
+
+                if (editText.length() != 0  && editEmail.length() !=0) {    //here
+                    AddData(newEntry1 , newEntry2);                         //here
+                    editText.setText("");
+
+                    editEmail.setText("");                              //here
+
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
+
+            }
+        });
+
+        btnViewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void AddData(String newEntry1 , String newEntry2) {
+        boolean insertData = mDatabaseHelper.addData(newEntry1 , newEntry2);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+}
